ba141382818466b5bbf0f672e24105b9f80a12a7
Steve Dower <steve.dower@microsoft.com>
2017-11-01 14:42:29 -0700

    Fixes VstsTaskSdk script Updates all task versions

--- update_vststasksdk.ps1
+++ update_vststasksdk.ps1
@@ -2,4 +2,15 @@ $mods = gci *\task.json | %{ Join-Path (Split-Path $_) "ps_modules" }
 
 $mods | ?{ Test-Path $_ } | %{ rmdir -Recurse -Force $_ }
 mkdir $mods | Out-Null
-$mods | %{ Save-Module -Name VstsTaskSdk -RequiredVersion 0.10.0 -Path $_ }
+
+$p = mkdir ps_modules -Force
+"Created $p..."
+
+Save-Module -Name VstsTaskSdk -RequiredVersion 0.10.0 -Path $p
+"Installed VstsTaskSdk to $p..."
+
+$mods | %{ copy "$p\VstsTaskSdk\0.10.0\*" (mkdir $_\VstsTaskSdk) }
+"Copied VstsTaskSdk to modules..."
+
+rmdir -Recurse -Force $p
+"Removed VstsTaskSdk module..."
